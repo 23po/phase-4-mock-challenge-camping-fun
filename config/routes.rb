52d5733cd2b4a3a1d3d9f2493a6cac3bb8539888
calvin <ckodalo@gmail.com>
2022-11-30 06:03:20 +0300

    commit tree 4bae7c099037a1559ac0fee91b7203bd84b2707a

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :activities, only: [:index, :show]
   end
   
-  resources :signups
-  resources :activities
+  resources :signups, only: [:create]
+  resources :activities, only: [:index, :destroy]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
